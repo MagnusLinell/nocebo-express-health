42d9c2c20ddbf3f5655b278d0d65fdc5e08814d1
Magnus Linell <magnus.linell@gmail.com>
2017-03-05 17:03:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Nocebo Express Health
+
+Handles express health.
+
+## Requirements
+
+Needs harmony to be set when used:
+
+    node --harmony
+
+## Setup
+
+Options:
+
+    pkg: String | package.json as text (required field name and version)
+
+Example:
+
+    {
+      name: "project-name",
+      version: "1.0.0"
+    }
+
+## Test
+
+To run unit tests:
+
+    npm test
---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+const validateOptions = (options) => {
+    if (!options || !options.pkg ) {
+        throw Error("argument {pkg: [package.json as text]} must be set");
+    }
+    if (!options.pkg.name) {
+        throw Error("argument {pkg: {name: [name of project]}} must be set");
+    }
+    if (!options.pkg.version) {
+        throw Error("argument {pkg: {version: [version of project]}} must be set");
+    }
+};
+
+const health = (options) => {
+    return (req, res, next) => {
+        res.json({
+            "status": "OK",
+            "name": options.pkg.name,
+            "version": options.pkg.version
+        });
+    }
+};
+
+module.exports = (options) => {
+    validateOptions(options);
+    return health(options);
+};
